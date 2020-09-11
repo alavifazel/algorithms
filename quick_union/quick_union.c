464e4f23419e3e29ef6d85503f1eaf3499d8f3ed
Iman Alavi Fazel <im.alavifazel@gmail.com>
2020-09-11 18:31:07 +0430

    Add quick-union in C

---
+++ quick_union/quick_union.c
@@ -0,0 +1,99 @@
+/*
+ * > Implementation of weighted quick-union with tree compression
+ *
+ * Integer array id[] of size maxSize holds the info about connections,
+ * in such a way that if node 'p' and 'u' are connected they have a same 'root id' in the array.
+ * Hence to check if node 'p' and 'u' are connected we just have to check if they have the same 'root id' value.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+	int *id;
+	int *depth_level;
+	int maxSize;
+} Union;
+
+void InitUnion(Union *u, int maxSize) {
+	int *id = (int*) malloc(sizeof(int) * maxSize); 	
+	int *depth_level = (int*) malloc(sizeof(int) * maxSize); 	
+	for(int i = 0; i < maxSize; ++i) {
+		id[i] = i;
+		depth_level[i] = 1;
+	}
+	u->id = id;
+	u->depth_level = depth_level;
+	u->maxSize = maxSize;
+}
+
+void UnionFree(Union *u) {
+	free(u->id);
+	free(u->depth_level);
+	u->maxSize = 0;
+}
+
+int UnionRoot(Union *u, int i) {
+	if(i > u->maxSize) {
+		fprintf(stderr, "Node number is exceeding the union's number of nodes!");
+		return -1;	
+	}
+	while(i != u->id[i]) {
+		// bellow code does not flattens (compresses) the tree completely.
+		// it merely makes the node to point to it's grandparent,
+		// but in practice it is almost as good as flattening the whole tree completely.
+		u->id[i] = u->id[u->id[i]]; 
+		i = u->id[i];
+	}
+	return i;
+}
+
+bool UnionIsConnected(Union *u, int a, int b) {
+	if(UnionRoot(u,a) == UnionRoot(u,b)) return true;
+	else return false;
+}
+
+void UnionConnect(Union *u, int a, int b) {
+	if(u->depth_level[a] > u->depth_level[b]) {
+		u->id[b] = UnionRoot(u, a);
+		u->depth_level[a] += u->depth_level[b]; 
+	} else {
+		u->id[a] = UnionRoot(u, b);
+		u->depth_level[b] += u->depth_level[a]; 
+	}
+}
+
+void UnionPrint(Union *u) {
+	printf("\n");
+	printf("  i   : ");
+	for(int i = 0; i < u->maxSize; ++i) {
+		printf(" %d", i);
+	}
+	printf("\n");
+	printf("id[i] : ");
+	for(int i = 0; i < u->maxSize; ++i) {
+		printf(" %d", u->id[i]);
+	}
+	printf("\n");
+}
+
+int main(){
+	Union u;
+	InitUnion(&u, 10);
+	UnionPrint(&u);
+
+	UnionConnect(&u,4,3);
+	UnionPrint(&u);
+
+	UnionConnect(&u,5,3);
+	UnionPrint(&u);
+
+	UnionConnect(&u,1,5);
+	UnionPrint(&u);
+
+	printf("%d\n", UnionIsConnected(&u,1,4)); // true!
+
+	UnionFree(&u);
+	return 0;
+}
