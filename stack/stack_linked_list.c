d9189e9ebdfa543fde98cfa17c4e219aa8e5a42a
Iman Alavi Fazel <im.alavifazel@gmail.com>
2020-09-16 06:02:58 +0430

    Add stack

---
+++ stack/stack_linked_list.c
@@ -0,0 +1,65 @@
+/*
+ * > Linked-list stack implementation
+ *   ( 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct Node Node;
+struct Node {
+	int data;
+	struct Node *next_node;	
+};
+
+typedef struct {
+	Node *node;	
+	int size;
+} Stack;
+
+void StackInit(Stack *s) {
+	s->node = NULL;
+	s->size = 0;
+}
+
+void StackPop(Stack *s) {
+	Node *new_node = s->node->next_node;
+	int new_node_data = s->node->next_node->data;
+	free(s->node);
+	s->node = new_node;
+	s->size--;
+}
+
+void StackFree(Stack *s) {
+	for(int i = 0; i < s->size - 1; ++i) 
+		StackPop(s);
+}
+
+void StackPush(Stack *s, int data) {
+	Node *old_node = s->node;
+	s->node = (Node*) malloc(sizeof(Node));	
+	s->node->data = data;
+	s->node->next_node = old_node;
+	s->size++;
+}
+
+bool StackIsEmpty(Stack *s) {
+	return s->size == 1; 
+}
+
+int main() { 
+	Stack s;
+	StackInit(&s);
+	StackPush(&s, 400);
+	StackPush(&s, 42);
+	StackPush(&s, 123123);
+	printf("%d\n", s.node->data);
+
+	StackPop(&s);
+	printf("%d\n", s.node->data);	
+
+	StackPop(&s);
+	printf("%d\n", s.node->data);	
+	
+	StackFree(&s);
+}
---
+++ stack/stack_resizing_arrays.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+	int *data;
+	int lastIndex, capacity;
+} Stack;
+
+void StackInit(Stack *s, int initialCapacity){
+	if(initialCapacity > 0) {
+		s->data = (int *) malloc(initialCapacity * sizeof(int));
+		s->lastIndex = 0;
+		s->capacity = initialCapacity;
+		memset(s->data, 0, s->capacity);
+	} else {
+	        fprintf(stderr, "Initial capacity should be positive!\n");
+		
+	}
+}
+
+void StackFree(Stack *s) {
+	free(s->data);
+	s->lastIndex = 0;
+}
+
+void StackPush(Stack *s, int key) {
+	if(s->lastIndex != s->capacity) {
+		s->lastIndex++;
+		s->data[s->lastIndex] = key;
+	}
+	else {
+		/* These lines should have been ideally in a private method named "resize".
+		 * Since C has no such encapsulation, publicly defining these
+		 * Lines into a public function could lead to errors.
+		 * In other words, only StackPush and StackPop should be able to the data capacity not the users themselves.
+		 */
+		int *tmp = realloc(s->data, s->capacity * 2);
+		if(tmp) {
+			s->capacity *= 2;
+			s->data = tmp;
+			s->lastIndex++;
+			s->data[s->lastIndex] = key;
+		} else {
+		      fprintf(stderr, "Can't inrease stack array size!\n");
+		}
+	}
+}
+
+int StackPop(Stack *s) {
+	int lastData = s->data[s->lastIndex];
+	s->data[s->lastIndex] = 0;
+	s->lastIndex--;
+
+	// Dividing by 4 prevents "thrashing"
+	if(s->lastIndex < s->capacity / 4) {
+		int *tmp = realloc(s->data, s->capacity / 2);
+		if(tmp) {
+			s->capacity /= 2;
+			s->data = tmp;
+			return lastData;
+		} else {
+		      fprintf(stderr, "Can't decrease stack array size!\n");
+		}
+	}
+}
+
+bool StackIsEmpty(Stack *s) {
+	return s->lastIndex == 1; 
+}
+
+int StackSize(Stack *s) {
+	return s->lastIndex;
+}
+
+int main() { 
+	Stack s;
+	StackInit(&s, 1);
+	StackPush(&s, 400);
+	StackPush(&s, 42);
+	StackPush(&s, 123123);
+	printf("%d\t%d\n",s.capacity, s.data[s.lastIndex]);
+	
+	StackPop(&s);
+	printf("%d\t%d\n",s.capacity, s.data[s.lastIndex]);
+
+	StackPop(&s);
+	printf("%d\t%d\n",s.capacity, s.data[s.lastIndex]);
+
+	StackPop(&s);
+	printf("%d\t%d\n",s.capacity, s.data[s.lastIndex]);
+	
+	StackFree(&s);
+}
