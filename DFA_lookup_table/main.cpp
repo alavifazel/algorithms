b9875a625c863911608d8a5dbf25211ad83d6c15
Iman Alavi Fazel <im.alavifazel@gmail.com>
2022-04-27 20:59:33 +0430

    Update DFA lookup table

--- DFA_lookup_table/main.cpp
+++ DFA_lookup_table/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <memory>
 #include <vector>
 #include <map>
 
 using std::vector;
 using std::map;
+using std::shared_ptr;
+using std::make_shared;
 
 template<class InputType>
 class DFA {
     public:
-        DFA(vector<map<InputType,int>*> states,  int state):
+        DFA(vector<shared_ptr<map<InputType,int>>> states,  int state):
             states{states},
             state(state) { }
 
@@ -23,19 +26,19 @@ class DFA {
 
     private:
         int state;
-        vector<map<InputType, int>*> states;
+        vector<shared_ptr<map<InputType,int>>> states;
 };
 
 int main() {
     using InputType = int;
-    vector<map<InputType, int>*> states;
+    vector<shared_ptr<map<InputType, int>>> states;
     map<InputType, int> lookupTableRow;
     lookupTableRow[0] = 1;
     lookupTableRow[1] = 2;
 
-    states.push_back(&lookupTableRow);
-    states.push_back(&lookupTableRow);
-    states.push_back(&lookupTableRow);
+    for(size_t i = 0; i < 3; ++i)
+        states.push_back(make_shared<map<InputType, int>>(lookupTableRow));
+
     DFA<int> dfa(states, 0);
 
     InputType input;
