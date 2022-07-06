a6c2ba65dd42f17516f4d5ea5568597db733a611
Iman Alavi Fazel <im.alavifazel@gmail.com>
2022-07-06 13:06:47 +0430

    Update dir fetch

--- dir_fetch (recursive)/main.js
+++ dir_fetch (recursive)/main.js
@@ -1,7 +1,9 @@
+const {log} = require('console');
 const fs = require('fs');
 
-async function readDir(dirs = '.', depth = 0) {
+async function readDir(dirs = ['.'], dirListLength = 3, depth = 0) {
 	try {
+        dirs = dirs.slice(0, dirListLength);
 		for(let i = 0; i < dirs.length; i++) {
 			let newDir = fs.readdirSync(dirs[i]);
 			for (let j = 0; j < newDir.length; j++) {
@@ -10,18 +12,25 @@ async function readDir(dirs = '.', depth = 0) {
 			if(depth == 0) {
 				console.log('\x1b[36m%s\x1b[0m', dirs[i]);
 			} else {
-				newDir.forEach(d => { 
+				for(let j = 0; j < Math.min(newDir.length, dirListLength); j++) {
 					process.stdout.write("\x1b[36m" + "*".repeat(depth) + " " + "\x1b[0m");
-					console.log(d) 
-				});
+					console.log(newDir[j]);
+                    log(j)
+				}
 			}
 			newDir = newDir.filter(newDir => fs.lstatSync(newDir).isDirectory() && newDir != '.' );
 
-			readDir(newDir, depth + 1);
+			readDir(newDir, dirListLength, depth + 1);
 
 		}
 	} catch (err) {
 		console.log(err);
 	}
 }
-readDir();
+
+let args = process.argv.slice(2);
+if(args.length > 2) {
+    console.info("Usage: node ./<script> <directory>='.' <dirListLength>=3");
+} else {
+    readDir(args[0], args[1]);
+}
